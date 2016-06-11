53588b315ccf3bb2209701f2085540e5781cd33a
Tony Bowden <tony@tmtm.com>
2016-06-11 08:05:29 +0100

    Get some from mt.wikipedia

--- scraper.rb
+++ scraper.rb
@@ -3,9 +3,11 @@
 
 require 'wikidata/fetcher'
 
-names = EveryPolitician::Wikidata.wikipedia_xpath( 
+en = EveryPolitician::Wikidata.wikipedia_xpath( 
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_parliament_of_Malta,_2013%E2%80%93',
   xpath: '//h2[span[contains(.,"List of MPs")]]/following-sibling::table[1]//tr[td]/td[2]//a[not(@class="new")]/@title',
 ) 
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: names })
+mt = WikiData::Category.new( 'Kategorija:Politiċi Maltin', 'mt').member_titles
+
+EveryPolitician::Wikidata.scrape_wikidata(names: { en: en, mt: mt })

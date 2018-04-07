94233d26288a5ac2e844b8512257cee453d0396e
Tony Bowden <tony@tmtm.com>
2018-04-07 10:40:05 +0100

    also fetch via SPARQL

--- scraper.rb
+++ scraper.rb
@@ -3,11 +3,13 @@
 
 require 'wikidata/fetcher'
 
-en = EveryPolitician::Wikidata.wikipedia_xpath( 
+en = EveryPolitician::Wikidata.wikipedia_xpath(
   url: 'https://en.wikipedia.org/wiki/List_of_members_of_the_parliament_of_Malta,_2013%E2%80%93',
   xpath: '//h2[span[contains(.,"List of MPs")]]/following-sibling::table[1]//tr[td]/td[2]//a[not(@class="new")]/@title',
-) 
-
+)
 mt = WikiData::Category.new( 'Kategorija:Politiċi Maltin', 'mt').member_titles
 
-EveryPolitician::Wikidata.scrape_wikidata(names: { en: en, mt: mt })
+query = 'SELECT DISTINCT ?item WHERE { ?item p:P39/ps:P39 wd:Q37857097 }'
+p39s = EveryPolitician::Wikidata.sparql(query)
+
+EveryPolitician::Wikidata.scrape_wikidata(ids: p39s, names: { en: en, mt: mt })
